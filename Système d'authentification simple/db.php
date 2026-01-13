60a9af7424135947048ea89c9ea8881c41274549
Thierry RAMANITRA <riad-design@gmx.com>
2026-01-13 22:23:56 +0100

    Remove password from database connection

--- Système d'authentification simple/db.php
+++ Système d'authentification simple/db.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=tp_auth;charset=utf8', 'root', 'Tongasoa');
+    $pdo = new PDO('mysql:host=localhost;dbname=tp_auth;charset=utf8', 'root', '');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
